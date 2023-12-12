040e3f2ac94ab43716c5290ee4177280cad817d7
FAHASENAFAISAL <fahasenafaisal@gmail.com>
2023-12-12 13:34:44 +0000

    Add bar chart

--- statistics&trends.py
+++ statistics&trends.py
@@ -10,20 +10,21 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 # Define file paths
-data_uri = 'C:\\Users\\USER\\Desktop\\ADS\\Assignment_2\\Dataset\\population.csv'
-pivoted_data_uri = 'C:\\Users\\USER\\Desktop\\ADS\\Assignment_2\\Dataset\\PivotedDataset.csv'
-cleaned_data_uri = 'C:\\Users\\USER\\Desktop\\ADS\\Assignment_2\\Dataset\\CleanedDataset.csv'
+population_data_path = 'C:\\Users\\USER\\Desktop\\ADS\\Assignment_2\\Dataset\\population.csv'
+pivoted_data_path = 'C:\\Users\\USER\\Desktop\\ADS\\Assignment_2\\Dataset\\PivotedDataset.csv'
+cleaned_data_path = 'C:\\Users\\USER\\Desktop\\ADS\\Assignment_2\\Dataset\\CleanedDataset.csv'
 
 # Function to load dataset
 def load_dataset(file_path):
     dataset = pd.read_csv(file_path)
     return dataset
 
-# Define feature codes, feature map, and country names
-feature_codes = ['Country Name', 'Country Code', 'Year', 'SP.POP.TOTL.MA.ZS', 'SP.POP.TOTL.FE.ZS', 'SP.POP.GROW',
-                 'SP.POP.DPND.YG', 'SP.POP.DPND.OL', 'SP.POP.DPND', 'SP.DYN.CDRT.IN', 'SP.DYN.CBRT.IN', 'SP.ADO.TFRT']
+# Define indicator codes, indicator map, and country names
+indicator_codes = ['Country Name', 'Country Code', 'Year', 'SP.POP.TOTL.MA.ZS', 'SP.POP.TOTL.FE.ZS', 'SP.POP.GROW',
+                 'SP.POP.DPND.YG', 'SP.POP.DPND.OL', 'SP.POP.DPND', 'SP.DYN.CDRT.IN', 'SP.DYN.CBRT.IN', 'SP.ADO.TFRT',
+                 'SP.POP.1564.TO.ZS','SH.DTH.IMRT']
 
-feature_map = {
+indicator_map = {
     "SP.POP.TOTL.MA.ZS": "Population, male (% of total population)",
     "SP.POP.TOTL.FE.ZS": "Population, female (% of total population)",
     "SP.POP.GROW": "Population growth (annual %)",
@@ -32,10 +33,12 @@ feature_map = {
     "SP.POP.DPND": "Age dependency ratio (% of working-age population)",
     "SP.DYN.CDRT.IN": "Death rate, crude (per 1,000 people)",
     "SP.DYN.CBRT.IN": "Birth rate, crude (per 1,000 people)",
-    "SP.ADO.TFRT": "Adolescent fertility rate (births per 1,000 women ages 15-19)"
+    "SP.ADO.TFRT": "Adolescent fertility rate (births per 1,000 women ages 15-19)",
+    "SP.POP.1564.TO.ZS":"Population ages 15-64 (% of total population)",
+    "SH.DTH.IMRT":"Number of infant deaths"
 }
 
-countries = {
+country_names = {
     "AUS": "Australia",
     "BHR": "Bahrain",
     "CAN": "Canada",
@@ -52,53 +55,58 @@ countries = {
 }
 
 # Load and melt the dataset
-melted_df = load_dataset(data_uri).melt(id_vars=['Country Name', 'Country Code', 'Indicator Name', 'Indicator Code'],
-                                        var_name='Year', value_name='Value')
+def load_and_melt_dataset(file_path):
+    dataset = load_dataset(file_path)
+    melted_df = dataset.melt(id_vars=['Country Name', 'Country Code', 'Indicator Name', 'Indicator Code'],
+                             var_name='Year', value_name='Value')
+    return melted_df
+
+melted_population_df = load_and_melt_dataset(population_data_path)
 
 # Pivot the table to have Indicator Codes as columns
-pivoted_df = melted_df.pivot_table(index=['Country Name', 'Country Code', 'Year'], columns='Indicator Code',
+pivoted_df = melted_population_df.pivot_table(index=['Country Name', 'Country Code', 'Year'], columns='Indicator Code',
                                    values='Value').reset_index()
 
 # Fill NaN values with 0
 pivoted_df.fillna(0, inplace=True)
 
 # Save the pivoted DataFrame to a CSV file
-pivoted_df.to_csv(pivoted_data_uri, index=False)
+pivoted_df.to_csv(pivoted_data_path, index=False)
 
-# Filter columns based on feature codes
-filtered_columns = [col for col in pivoted_df.columns if col in feature_codes]
-df_filtered = pivoted_df[filtered_columns]
+# Filter columns based on indicator codes
+filtered_columns = [col for col in pivoted_df.columns if col in indicator_codes]
+filtered_df = pivoted_df[filtered_columns]
 
 # Clean the transformed dataset
 # Fill missing values with the mean of the column
-df_filtered = df_filtered.fillna(df_filtered.mean(numeric_only=True))
-df_filtered.to_csv(cleaned_data_uri)
+cleaned_df = filtered_df.fillna(filtered_df.mean(numeric_only=True))
+cleaned_df.to_csv(cleaned_data_path)
 
 # Apply statistical methods on the cleaned dataset
-copy_df_cleaned = df_filtered.drop(['Year', 'Country Name'], axis='columns')
-print(copy_df_cleaned.describe())
+statistical_df = cleaned_df.drop(['Year', 'Country Name'], axis='columns')
+print(statistical_df.describe())
 
 # Create DataFrames for specific countries
-df_argentina = df_filtered[df_filtered["Country Name"] == "Argentina"]
-df_japan = df_filtered[df_filtered["Country Name"] == "Japan"]
-df_sweden = df_filtered[df_filtered["Country Name"] == "Sweden"]
+df_argentina = cleaned_df[cleaned_df["Country Name"] == "Argentina"]
+df_japan = cleaned_df[cleaned_df["Country Name"] == "Japan"]
+df_sweden = cleaned_df[cleaned_df["Country Name"] == "Sweden"]
 
 # Correlation Matrix and Heat map for Argentina
-correlation_matrix_argentina = df_argentina.corr(numeric_only=True).rename(columns=feature_map).rename(index=feature_map)
+correlation_matrix_argentina = df_argentina.corr(numeric_only=True).rename(columns=indicator_map).rename(index=indicator_map)
 plt.figure(figsize=(10, 10))
 sns.heatmap(correlation_matrix_argentina, annot=True, fmt=".1g", vmax=1, vmin=0)
 plt.title('Correlation Matrix for Argentina')
 plt.show()
 
 # Correlation Matrix and Heat map for Japan
-correlation_matrix_japan = df_japan.corr(numeric_only=True).rename(columns=feature_map).rename(index=feature_map)
+correlation_matrix_japan = df_japan.corr(numeric_only=True).rename(columns=indicator_map).rename(index=indicator_map)
 plt.figure(figsize=(10, 10))
 sns.heatmap(correlation_matrix_japan, annot=True, fmt=".1g", vmax=1, vmin=0)
 plt.title('Correlation Matrix for Japan')
 plt.show()
 
 # Correlation Matrix and Heat map for Sweden
-correlation_matrix_sweden = df_sweden.corr(numeric_only=True).rename(columns=feature_map).rename(index=feature_map)
+correlation_matrix_sweden = df_sweden.corr(numeric_only=True).rename(columns=indicator_map).rename(index=indicator_map)
 plt.figure(figsize=(10, 10))
 sns.heatmap(correlation_matrix_sweden, annot=True, fmt=".1g", vmax=1, vmin=0)
 plt.title('Correlation Matrix for Sweden')
@@ -106,15 +114,98 @@ plt.show()
 
 # Bar graphs
 # Filtering for specific countries and years
-df_population_by_year = pd.read_csv(cleaned_data_uri)
+df_cleaned = pd.read_csv(cleaned_data_path)
 
-filtered_population = df_population_by_year[
-    ((df_population_by_year['Country Name'] == 'Bahrain') | (df_population_by_year['Country Name'] == 'Canada') |
-     (df_population_by_year['Country Name'] == 'Sweden') | (df_population_by_year['Country Name'] == 'Uganda')) &
-    ((df_population_by_year['Year'] == 1980) | (df_population_by_year['Year'] == 2000) | (df_population_by_year['Year'] == 2022))]
+filtered_population = df_cleaned[
+    ((df_cleaned['Country Name'] == 'Argentina') | (df_cleaned['Country Name'] == 'Japan') |
+     (df_cleaned['Country Name'] == 'Canada') | (df_cleaned['Country Name'] == 'Uganda')) &
+    ((df_cleaned['Year'] == 1975) | (df_cleaned['Year'] == 1985) | 
+     (df_cleaned['Year'] == 1995) | (df_cleaned['Year'] == 2005))]
 
-filtered_population = filtered_population[["Country Name", "Year", "SP.POP.GROW"]]
-pivoted_population_df = filtered_population.pivot(index='Country Name', columns='Year', values='SP.POP.GROW').reset_index()
-pivoted_population_df.plot(kind='bar', x='Country Name', y=[1980, 2000, 2022])
+filtered_population = filtered_population[["Country Name", "Year", "SP.POP.GROW","SP.DYN.CBRT.IN","SH.DTH.IMRT"]]
+
+# Pivot the data for first bar plot
+pivoted_population_growth_df = filtered_population.pivot(index='Country Name', columns='Year', values='SP.POP.GROW').reset_index()
+
+# Plotting the bar graph for population growth
+ax = pivoted_population_growth_df.plot(kind='bar', x='Country Name', y=[1975, 1985, 1995, 2005], rot=30, legend=True)
 plt.xticks(rotation=30, horizontalalignment="center")
+
+# Adding labels and title for population growth
+ax.set_ylabel('Population Growth (annual %)')
+ax.set_xlabel('Country')
+plt.title('Population Growth Annually')
+
+# Show the plot
+plt.show()
+
+# Pivot the data for second bar plot
+pivoted_birth_rate_df = filtered_population.pivot(index='Country Name', columns='Year', values='SP.DYN.CBRT.IN').reset_index()
+
+# Plotting the bar graph for birth rate
+ax = pivoted_birth_rate_df.plot(kind='bar', x='Country Name', y=[1975, 1985, 1995, 2005], rot=30, legend=True)
+plt.xticks(rotation=30, horizontalalignment="center")
+
+# Adding labels and title for birth rate
+ax.set_ylabel('Birth Rate (crude per 1,000 people)')
+ax.set_xlabel('Country')
+plt.title('Crude Birth Rate Per 1,000 People')
+
+# Show the plot
+plt.show()
+
+# Bar plot for infant death
+# Pivot the data for third bar plot
+pivoted_infant_death_df = filtered_population.pivot(index='Country Name', columns='Year', values='SH.DTH.IMRT').reset_index()
+
+# Plotting the bar graph for infant death
+ax = pivoted_infant_death_df.plot(kind='bar', x='Country Name', y=[1975, 1985, 1995, 2005], rot=30, legend=True)
+plt.xticks(rotation=0, horizontalalignment="center")
+
+# Adding labels and title for infant death
+ax.set_xlabel('Country')
+plt.title('Infant Death Rate')
+
+# Show the plot
+plt.show()
+
+# Line graph for Age dependency ratio (% of working-age population)
+# Filter data for specific countries
+working_age_argentina = df_cleaned[df_cleaned['Country Name'] == "Argentina"]
+working_age_japan = df_cleaned[df_cleaned['Country Name'] == "Japan"]
+working_age_sweden = df_cleaned[df_cleaned['Country Name'] == "Sweden"]
+working_age_uganda = df_cleaned[df_cleaned['Country Name'] == "Uganda"]
+
+# Plotting
+plt.plot(working_age_argentina["Year"], working_age_argentina["SP.POP.DPND"], label='Argentina')
+plt.plot(working_age_japan["Year"], working_age_japan["SP.POP.DPND"], label='Japan')
+plt.plot(working_age_sweden["Year"], working_age_sweden["SP.POP.DPND"], label='Canada')
+plt.plot(working_age_uganda["Year"], working_age_uganda["SP.POP.DPND"], label='Uganda')
+
+# Adding labels and title
+plt.xlabel('Year')
+plt.ylabel('Age Dependency Ratio (% of Working-Age Population)')
+plt.title('Age Dependency Ratio Comparison')
+
+# Adding legend
+plt.legend()
+
+# Display the plot
+plt.show()
+
+# Line graph for Population ages 15-64 (% of total population)
+plt.plot(working_age_argentina["Year"], working_age_argentina["SP.POP.1564.TO.ZS"], label='Argentina')
+plt.plot(working_age_japan["Year"], working_age_japan["SP.POP.1564.TO.ZS"], label='Japan')
+plt.plot(working_age_sweden["Year"], working_age_sweden["SP.POP.1564.TO.ZS"], label='Sweden')
+plt.plot(working_age_uganda["Year"], working_age_uganda["SP.POP.1564.TO.ZS"], label='Uganda')
+
+# Adding labels and title
+plt.xlabel('Year')
+plt.ylabel('Population of 15-64 Age Group (% of Total Population)')
+plt.title('Population of Working-Age People')
+
+# Adding legend
+plt.legend()
+
+# Display the plot
 plt.show()
